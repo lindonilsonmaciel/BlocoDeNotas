239b76c84c7d43b8a9497e4068aae54018168f79
Lindonilson Maciel <lindonilsonmaciel@gmail.com>
2019-10-04 10:56:19 -0300

    métodos salvar como, salvar, copiar, colar, recortar e sair

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -37,3 +37,63 @@ void MainWindow::on_actionAbrir_triggered(){
     ui->textEdit->setText(texto);
     arquivo.close();
 }
+
+void MainWindow::on_actionSalvar_como_triggered(){
+    QString filtro = "Todos os Arquivos (*.*);;Arquivos de Texto (*.txt)";
+    //Posso passar apenas os dois primeiros parâmetros se preferir
+    QString nome_arquivo = QFileDialog::getSaveFileName(this, "Salvar", QDir::homePath(), filtro);
+    QFile arquivo(nome_arquivo);
+    localArquivo = nome_arquivo;
+    if(!arquivo.open(QFile::WriteOnly | QFile::Text)){
+        QMessageBox::warning(this, "Erro", "O arquivo não pode ser salvo.");
+        return;
+    }
+    QTextStream saida(&arquivo);
+    QString texto = ui->textEdit->toPlainText();
+    saida << texto;
+    arquivo.flush();
+    arquivo.close();
+}
+
+void MainWindow::on_actionSalvar_triggered(){
+    QFile arquivo(localArquivo);
+    if(!arquivo.open(QFile::WriteOnly | QFile::Text)){
+        QMessageBox::warning(this, "Erro", "O arquivo não pode ser salvo.");
+        return;
+    }
+    QTextStream saida(&arquivo);
+    QString texto = ui->textEdit->toPlainText();
+    saida << texto;
+    arquivo.flush();
+    arquivo.close();
+}
+
+void MainWindow::on_actionSair_triggered(){
+    //Fechar
+    close();
+}
+
+void MainWindow::on_actionCopiar_triggered(){
+    //Copiar
+    ui->textEdit->copy();
+}
+
+void MainWindow::on_actionRecortar_triggered(){
+    //Recortar
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_actionColar_triggered(){
+    //Colar
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_actionDesfazer_triggered(){
+    //Desfazer
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_actionRefazer_triggered(){
+    //Refazer
+    ui->textEdit->redo();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -20,6 +20,22 @@ private slots:
 
     void on_actionAbrir_triggered();
 
+    void on_actionSalvar_como_triggered();
+
+    void on_actionSalvar_triggered();
+
+    void on_actionSair_triggered();
+
+    void on_actionCopiar_triggered();
+
+    void on_actionRecortar_triggered();
+
+    void on_actionColar_triggered();
+
+    void on_actionDesfazer_triggered();
+
+    void on_actionRefazer_triggered();
+
 private:
     Ui::MainWindow *ui;
     QString localArquivo;
